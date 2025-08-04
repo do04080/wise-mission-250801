31d838107b6641c74d1bf3dbe0d21a5fc3afb42f
[BE9]도석환 <do04080@gmail.com>
2025-08-04 10:28:04 +0900

    5단계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,25 +1,37 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("== 명언 앱==");
 
-        String content="";
-        String author="";
+        List<WiseSaying> list = new ArrayList<>();
 
+        int count=0;
         while (true) {
             System.out.print("명령)");
             String command=sc.nextLine();
             if(command.equals("등록")) {
                 System.out.print("명언 : ");
-                content=sc.nextLine();
+                String content=sc.nextLine();
                 System.out.print("작가 : ");
-                author=sc.nextLine();
-            } else if(command.equals("종료")) break;
+                String author=sc.nextLine();
+                count++;
+                list.add(new WiseSaying(count,content,author));
+                System.out.println(count+"번 명언이 등록되었습니다.");
+            } else if(command.equals("목록")){
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("-------------------");
+                for(int i=0;i<list.size();i++){
+                    System.out.println("%d / %s / %s " .formatted(list.get(i).num,list.get(i).author,list.get(i).content));
+                }
+            }
+            else if(command.equals("종료")) break;
         }
     }
 }
---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class WiseSaying {
+    public String content;
+    public String author;
+    public int num;
+    public WiseSaying(int num,String content,String author){
+        this.num=num;
+        this.content=content;
+        this.author=author;
+    }
+}
